94b797be0f90c408c3970aaa40f7d0c882cfa994
Juan David Gómez S <75592506+JuandGomezS@users.noreply.github.com>
2023-02-20 19:47:04 -0500

    Update mysql.options.js

--- src/options/mysql.options.js
+++ src/options/mysql.options.js
@@ -6,10 +6,10 @@ dotenv.config()
 export const options = {
     client: 'mysql',
     connection: {
-        host: process.env.IP,
-        user: process.env.USER,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     },
     debug: true,
     log: {
