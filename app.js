65b177ee9e6d4acbb900b514a6aefbcc2fc98a59
Juan David Gómez S <75592506+JuandGomezS@users.noreply.github.com>
2023-02-20 19:41:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ export function startServer(port) {
     const httpServer = createServer(app);
     const io = new Server(httpServer);
 
-    const PORT = port;
+    const PORT = port || process.env.PORT;
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(express.static('public'));
